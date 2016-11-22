34fb2700d4e41cb470c091f5d21fdcd74f75b8a9
Wei Wang <wvw@google.com>
2016-11-22 01:58:00 +0000

    lettuce: Accelerate boot by paralleling device enable
    Some QCOM devices require sysfs to trigger boot/init which are blocking
    the init process.
    [   12.276506] init: Command 'write /sys/kernel/boot_adsp/boot 1' action=early-boot (/vendor/etc/init/hw/init.qcom.rc:58) returned 0 took 1066ms.
    This CL is to put those slow to start devices in a
    separate service and wait for the service to be done later on.
    
    Signed-off-by: Ad!thya R <gh0strider.2k18.reborn@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -35,6 +35,22 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.devstart.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.qcom.devstart.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.devwait.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := bin/init.qcom.devwait.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
