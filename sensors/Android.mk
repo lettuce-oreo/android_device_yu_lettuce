00eedf8471d5f7089ebe0fbf965f60f5a7fafaec
nicknitewolf <nicknitewolf@lineageos.org>
2017-08-17 00:50:10 +0800

    lettuce: sensors: Cleanup sensor HAL
    Change-Id: I8496c6bc124f90606e8f435863f07dbac230f197
    Signed-off-by: Ad!thya R <gh0strider.2k18.reborn@gmail.com>

--- sensors/Android.mk
+++ sensors/Android.mk
@@ -2,33 +2,34 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := sensors.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := \
+    sensors.cpp \
+    SensorBase.cpp \
+    LightSensor.cpp \
+    ProximitySensor.cpp \
+    CompassSensor.cpp \
+    Accelerometer.cpp \
+    Gyroscope.cpp \
+    InputEventReader.cpp \
+    CalibrationManager.cpp \
+    NativeSensorManager.cpp \
+    VirtualSensor.cpp \
+    sensors_XML.cpp
 
 LOCAL_CFLAGS += -DLOG_TAG=\"Sensors\"
 
-LOCAL_C_INCLUDES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+LOCAL_C_INCLUDES := \
+    external/libxml2/include \
+    external/icu/icu4c/source/common
+
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
-LOCAL_SRC_FILES :=	\
-		sensors.cpp 			\
-		SensorBase.cpp			\
-		LightSensor.cpp			\
-		ProximitySensor.cpp		\
-		CompassSensor.cpp		\
-		Accelerometer.cpp				\
-		Gyroscope.cpp				\
-		InputEventReader.cpp \
-		CalibrationManager.cpp \
-		NativeSensorManager.cpp \
-		VirtualSensor.cpp	\
-		sensors_XML.cpp
-
-LOCAL_C_INCLUDES += external/libxml2/include	\
-		    external/icu/icu4c/source/common
-
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libutils
+
+LOCAL_MODULE := sensors.msm8916
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -36,11 +37,11 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := libcalmodule_common
 LOCAL_SRC_FILES := \
-		   algo/common/common_wrapper.c \
-		   algo/common/compass/AKFS_AOC.c \
-		   algo/common/compass/AKFS_Device.c \
-		   algo/common/compass/AKFS_Direction.c \
-		   algo/common/compass/AKFS_VNorm.c
+    algo/common/common_wrapper.c \
+    algo/common/compass/AKFS_AOC.c \
+    algo/common/compass/AKFS_Device.c \
+    algo/common/compass/AKFS_Direction.c \
+    algo/common/compass/AKFS_VNorm.c
 
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_TAGS := optional
