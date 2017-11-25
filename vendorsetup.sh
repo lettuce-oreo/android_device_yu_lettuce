764d42fce1764d1131634f225d815d0c695efbac
Joe Maples <joe@frap129.org>
2017-11-24 21:14:57 -0500

    lettuce: sdclang: -fvectorize-loops is now -fvectorize
    Clang 4.0 changed the argument

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,6 +2,6 @@ sh device/yu/lettuce/patches/apply.sh
 croot
 
 export TARGET_USE_SDCLANG=true
-export SDCLANG_FLAGS="-O3 -fvectorize-loops -Wno-vectorizer-no-neon -Wno-user-defined-warnings"
-export SDCLANG_FLAGS_2="-O3 -fvectorize-loops -Wno-vectorizer-no-neon -Wno-user-defined-warnings"
-export SDCLANG_COMMON_FLAGS="-O3 -fvectorize-loops -Wno-vectorizer-no-neon -Wno-user-defined-warnings"
+export SDCLANG_FLAGS="-O3 -fvectorize -Wno-vectorizer-no-neon -Wno-user-defined-warnings"
+export SDCLANG_FLAGS_2="-O3 -fvectorize -Wno-vectorizer-no-neon -Wno-user-defined-warnings"
+export SDCLANG_COMMON_FLAGS="-O3 -fvectorize -Wno-vectorizer-no-neon -Wno-user-defined-warnings"
