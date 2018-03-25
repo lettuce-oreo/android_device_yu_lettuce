24ff131beab5c754af6d327237e6553411217358
dianlujitao <dianlujitao@lineageos.org>
2018-03-25 17:16:17 +0800

    lettuce: Switch to binderized QTI power HAL
    Change-Id: I82e8386c2f3f02f408f5ac374e6f1c486b94bb29

--- device.mk
+++ device.mk
@@ -218,11 +218,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    power.msm8916
-
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.0-service-qti
 
 # Radio
 PRODUCT_PACKAGES += \
