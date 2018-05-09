8153ac472780823a249e72aa2f16f30fab462dde
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-05-09 04:10:40 +0000

    lettuce: config.fs: Add filesystem config for IMS daemons
    * Originally from c40051cdc60c82309d938a304176f4dbfac30d59 in CAF device/qcom/common

---
+++ fs_config/config.fs
@@ -0,0 +1,23 @@
+[vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
