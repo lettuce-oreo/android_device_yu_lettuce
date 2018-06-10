a0074ae8092de3e9db803715fcc761782a4d67b6
althafvly <althafvly@gmail.com>
2018-06-10 21:45:27 +0530

    lettuce: Build GNSS HIDL impl
    Change-Id: I83aec8b57f18383642d83110a8720fdff1b92546
    Signed-off-by: Ad!thya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -114,7 +114,9 @@ PRODUCT_PACKAGES + \
     libgnss \
     libgnsspps
 
+# GNSS HAL
 PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.0-impl \
     android.hardware.gnss@1.0-impl-qti \
     android.hardware.gnss@1.0-service-qti
 
