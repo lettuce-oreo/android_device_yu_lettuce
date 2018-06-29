ee4dd0ce01ffe55c94a8014a792cd722fae25ec7
Bruno Martins <bgcngm@gmail.com>
2018-06-29 16:00:43 +0100

    lettuce: ril: Protect QTI specific broadcasts
    Change-Id: I3561252c112582a3d1f4210b29046287a6aeb650
    Signed-off-by: Ad!thya R <gh0strider.2k18.reborn@gmail.com>

--- ril/Android.mk
+++ ril/Android.mk
@@ -17,6 +17,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_FULL_LIBS_MANIFEST_FILES := $(LOCAL_PATH)/AndroidManifest.xml
 LOCAL_MODULE := qti-telephony-common
 LOCAL_JAVA_LIBRARIES := telephony-common telephony-ext
 LOCAL_SRC_FILES := $(call all-java-files-under,src)
