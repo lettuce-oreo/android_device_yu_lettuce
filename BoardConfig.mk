2e7f16792e0ca8b5e65c3cde4522f8b12c01da63
Rahul Singh <rahul99jaykar00@gmail.com>
2018-07-09 07:16:17 +0530

    Revert "lettuce: Drop usage of FORCE_32_BIT"
    This reverts commit 05526ceeeb53da2e19e2a0e099cc68e105acfe49.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,7 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 TARGET_NO_BOOTLOADER := true
 
 # Architecture
+ifneq ($(FORCE_32_BIT),true)
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
@@ -35,6 +36,15 @@ TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
+else
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+
+TARGET_USES_64_BIT_BINDER := true
+endif
 
 # Audio
 AUDIO_FEATURE_ENABLED_KPI_OPTIMIZE := true
