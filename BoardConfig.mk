56ef7970d4e54d4cab075694aab1f2b5d00aad3d
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-07-26 22:26:12 +0530

    lettuce: Nuke IMS camera and boringssl shim
    * Not required with T88Q_14 blobs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,8 +195,6 @@ BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/vendor/lib64/lib-imsdpl.so|libshims_boringssl.so \
-    /system/vendor/lib64/lib-imscamera.so|libshims_camera.so \
     /system/vendor/lib64/lib-imsvt.so|libshims_ims.so \
     /system/vendor/lib64/libflp.so|libshims_flp.so \
     /system/vendor/lib64/libizat_core.so|libshims_get_process_name.so \
