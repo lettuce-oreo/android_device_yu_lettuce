2fa4c962bfa39d34a595cf3f1962b38d0f7b0f70
LuK1337 <priv.luk@gmail.com>
2018-08-11 04:50:00 +0530

    lettuce: Disable broken HALs

--- device.mk
+++ device.mk
@@ -72,9 +72,9 @@ PRODUCT_PACKAGES += \
     libmm-qcamera \
     Snap
 
-PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@3.2-impl
+#PRODUCT_PACKAGES += \
+#    android.hardware.camera.provider@2.4-impl \
+#    camera.device@3.2-impl
 
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
@@ -110,9 +110,9 @@ PRODUCT_PACKAGES += \
     libshims_get_process_name \
     libshims_flp
 
-PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl \
-    android.hardware.gnss@1.0-service
+#PRODUCT_PACKAGES += \
+#    android.hardware.gnss@1.0-impl \
+#    android.hardware.gnss@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
