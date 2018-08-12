4eb5ec35bf4d4b8969445e1ef25b3a19657cd113
abhinavgupta371 <abhinavgupta371@gmail.com>
2018-08-12 08:56:41 +0530

    Revert "lettuce: Add Doze Service and Ambient Display."
    This reverts commit 85b813c5aee2e0ac9c811af663cabf1733251228.

--- device.mk
+++ device.mk
@@ -95,10 +95,6 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service
 
-# Doze
-PRODUCT_PACKAGES += \
-    YUDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl
